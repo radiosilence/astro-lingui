d4cefa9b995c5f2fb080506288e734ca83baff90
James Cleveland <jc@blit.cc>
2025-10-01 20:33:29 +0100

    hahaha

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import type { ExtractorType } from "@lingui/conf";
-import type { AstroIntegration } from "astro";
 import { extractor } from "./extractor";
 import { integration } from "./integration";
 
 const astroLingui: {
-  extractor: ExtractorType;
-  integration: (...args: any[]) => AstroIntegration;
+  extractor: typeof extractor;
+  integration: typeof integration;
 } = {
   extractor,
   integration,
